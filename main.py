784cda54d48c2297d909a8816e228f349559f6cd
dkozerev <149874795+dkozerev@users.noreply.github.com>
2024-03-02 13:22:11 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,22 +1,63 @@
 import pygame
+
 pygame.init()
 
-
-win_w = 700
-win_h = 500
-FPS = 40
-
-window = pygame.display.set_mode((win_w,win_h))
-
+scr_width = 500
+scr_height = 500
+screen = pygame.display.set_mode((scr_width, scr_height))
+pygame.display.set_caption("Ping Pong")
+# создаём таймер
 clock = pygame.time.Clock()
-pygame.display.set_caption("PingPong")
+
+FPS = 50
+
+screen.fill((20, 180, 255))
+
+class GameSprite:
+    def __init__(self, x, y, w, h, image):
+        self.rect = pygame.Rect(x, y, w, h)
+        self.image = pygame.transform.scale(image, 
+                                (self.rect.width, self.rect.height))     
+    
+    def draw(self):
+        screen.blit(self.image, (self.rect.x, self.rect.y))
+
+class Ball(GameSprite):
+    def __init__(self, x, y, w, h, image, speed_x, speed_y):
+        super().__init__(x, y, w, h, image)
+        self.speed_x = speed_x
+        self.speed_y = speed_y
+    
+    def move(self):
+        #мяч движется в зависимости от скорости
+        self.rect.x += self.speed_x
+        self.rect.y += self.speed_y
+        # если касается верхней границы, скорость по вертикали меняет знак
+        if self.rect.y >= scr_height - self.rect.height:
+            self.speed_y *= (-1)
+        # если касается нижней границы, скорость по вертикали меняет знак
+        if self.rect.y <= 0:
+            self.speed_y *= (-1)
+
+class Player(GameSprite):
+    def __init__(self, x, y, w, h, image, speed, key_up, key_down):
+        super().__init__(x, y, w, h, image)
+        self.speed = speed
+        self.key_down = key_down
+        self.key_up = key_up
+    
+    def move(self):
+        if pygame.key.get_pressed()[self.key_up]:
+           self.rect.y -= self.speed
+        if pygame.key.get_pressed()[self.key_down]:
+           self.rect.y += self.speed
 
 game = True
 while game:
-    window.fill((40,150,255))
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             game = False
+    
     pygame.display.update()
     clock.tick(FPS)
-
